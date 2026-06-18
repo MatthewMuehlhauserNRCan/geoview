9a5d50d434150bbd197cb7c00db0cee41af72494
Matthew Muehlhauser <matthew.muehlhauser@nrcan-rncan.gc.ca>
2026-06-18 11:23:59 -0400

    Updates

--- workers/fetch-esri-worker-script.worker.js
+++ workers/fetch-esri-worker-script.worker.js
@@ -1,4 +1,4 @@
-/*! Package:fetch-esri-worker-script: 2.2.0 - "b705afe9da95ac8fb5a28083e2a276ebf59f587d" - 2026-06-18T14:56:23.851Z */(()=>{"use strict";
+/*! Package:fetch-esri-worker-script: 2.2.0 - "b705afe9da95ac8fb5a28083e2a276ebf59f587d" - 2026-06-18T15:20:28.176Z */(()=>{"use strict";
 /**
  * @license
  * Copyright 2019 Google LLC
